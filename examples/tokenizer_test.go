43056468b438cd6bbdb33a0bf05b130b79bce76d
ofabricio <fabriciogiac@gmail.com>
2026-09-01 09:50:38 -0300

    add tokenizer

--- examples/tokenizer_test.go
+++ examples/tokenizer_test.go
@@ -2,42 +2,35 @@ package lang
 
 import (
 	"fmt"
+	"regexp"
 
 	. "github.com/ofabricio/nom"
 )
 
-// Example_tokenizer shows how to tokenize a string into individual tokens.
-func Example_tokenizer() {
+// ExampleTokenize shows how to split a string into individual tokens.
+func ExampleTokenize() {
 
 	src := `
 		fun main () {
 			one();
 		}
-
-		let x = 1
 	`
 
-	p := New(src)
-	for p.Optional(WS) && p.More() {
-		if m := p.Mark(); p.Match(WORD) || p.Any() {
-			t := p.Token(m)
-			fmt.Printf("Idx: %2v, Row: %1v, Col: %2v, Text: %v\n", t.Idx, t.Row, t.Col, t.Text)
-		}
+	SYMB := regexp.MustCompile(`^[^\s]`)
+
+	for kind, t := range Tokenize(src, T("Word", WORD), T("Symb", SYMB)) {
+		fmt.Printf("Kind: %v, Idx: %2v, Row: %1v, Col: %2v, Text: %v\n", kind, t.Idx, t.Row, t.Col, t.Text)
 	}
 
 	// Output:
-	// Idx:  3, Row: 2, Col:  3, Text: fun
-	// Idx:  7, Row: 2, Col:  7, Text: main
-	// Idx: 12, Row: 2, Col: 12, Text: (
-	// Idx: 13, Row: 2, Col: 13, Text: )
-	// Idx: 15, Row: 2, Col: 15, Text: {
-	// Idx: 20, Row: 3, Col:  4, Text: one
-	// Idx: 23, Row: 3, Col:  7, Text: (
-	// Idx: 24, Row: 3, Col:  8, Text: )
-	// Idx: 25, Row: 3, Col:  9, Text: ;
-	// Idx: 29, Row: 4, Col:  3, Text: }
-	// Idx: 34, Row: 6, Col:  3, Text: let
-	// Idx: 38, Row: 6, Col:  7, Text: x
-	// Idx: 40, Row: 6, Col:  9, Text: =
-	// Idx: 42, Row: 6, Col: 11, Text: 1
+	// Kind: Word, Idx:  3, Row: 2, Col:  3, Text: fun
+	// Kind: Word, Idx:  7, Row: 2, Col:  7, Text: main
+	// Kind: Symb, Idx: 12, Row: 2, Col: 12, Text: (
+	// Kind: Symb, Idx: 13, Row: 2, Col: 13, Text: )
+	// Kind: Symb, Idx: 15, Row: 2, Col: 15, Text: {
+	// Kind: Word, Idx: 20, Row: 3, Col:  4, Text: one
+	// Kind: Symb, Idx: 23, Row: 3, Col:  7, Text: (
+	// Kind: Symb, Idx: 24, Row: 3, Col:  8, Text: )
+	// Kind: Symb, Idx: 25, Row: 3, Col:  9, Text: ;
+	// Kind: Symb, Idx: 29, Row: 4, Col:  3, Text: }
 }
---
+++ tokenizer.go
@@ -0,0 +1,36 @@
+package nom
+
+import (
+	"iter"
+)
+
+// Tokenize splits the input string into tokens based on the provided tokenizers.
+func Tokenize[K any](src string, match ...Tokenizer[K]) iter.Seq2[K, Token] {
+	return func(yield func(K, Token) bool) {
+		p := New(src)
+	outer:
+		for p.More() {
+			mrk := p.Mark()
+			for _, m := range match {
+				if k, ok := m(&p); ok {
+					if !yield(k, p.Token(mrk)) {
+						return
+					}
+					continue outer
+				}
+			}
+			p.Next()
+		}
+	}
+}
+
+// T matches and identifies a token.
+func T[MT MatchType, Kind any](kind Kind, v MT) Tokenizer[Kind] {
+	return func(s *Parser) (Kind, bool) {
+		return kind, s.Match(v)
+	}
+}
+
+// Tokenizer is a function that attempts to match a
+// token and returns its kind and success status.
+type Tokenizer[Kind any] func(s *Parser) (Kind, bool)
